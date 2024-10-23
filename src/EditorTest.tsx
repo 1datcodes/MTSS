90743ede438037d52a329ebb3c2fe6e233a6713a
JesseIguess <blueberry21007@gmail.com>
2024-10-23 21:37:59 +0000

    :)

--- src/EditorTest.tsx
+++ src/EditorTest.tsx
@@ -3,6 +3,11 @@ import { useRef } from "react";
 function EditorTest() {
     const contentEditableRef = useRef<HTMLDivElement>(null);
 
+    let saveData = {
+        "title": "Untitled",
+        "content": ""
+    }
+
     const applyStyle = (tag: string) => {
         const selection = document.getSelection();
         if (!selection || selection.rangeCount === 0) return;
@@ -13,12 +18,31 @@ function EditorTest() {
         span.appendChild(selectedText);
         range.insertNode(span);
     };
+    const generateBlogId = () => {
+        return "blog_" + btoa((Date.now() << localStorage.length).toString());
+    }
+    const save = (id: string) => {
+        localStorage.setItem(id, JSON.stringify(saveData));
+    }
+    const updateTitle = (event: Event) => {
+        saveData.title = event.target.value;
+    }
+    const updateContent = (mutationsList: MutationRecord[]) => {
+        alert(JSON.stringify(mutationsList))
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                console.log('Content changed to:', document.getElementById("editor").innerHTML);
+            }
+        }
+    }
+    const contentObserver = new MutationObserver(updateContent);
 
     return (
         <div>
             <link rel="stylesheet" href="./index.css" />
 
-            <input type="text" placeholder="enter title lol" />
+            <h1><input type="text" placeholder="enter title lol" onChange={updateTitle} class="title-editor" /></h1>
+            
             <br />
             <div className="toolbar">
                 <button onClick={() => applyStyle('b')}><b>B</b></button>
@@ -26,7 +50,7 @@ function EditorTest() {
                 <button onClick={() => applyStyle('u')}><u>U</u></button>
             </div>
             <br />
-            <div ref={contentEditableRef} contentEditable={true}></div>
+            <div ref={contentEditableRef} contentEditable={true} id="editor"></div>
         </div>
     )
 }
