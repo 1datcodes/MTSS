67e66d0c60c2369984c8c6447178fc5452e38ba8
Michi Tanaka <tanaka.michi@icloud.com>
2024-10-23 13:48:27 -0700

    Buttons work

--- src/EditorTest.tsx
+++ src/EditorTest.tsx
@@ -1,15 +1,22 @@
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 
 function EditorTest() {
+    const contentEditableRef = useRef<HTMLDivElement>(null);
+
+    const execCommand = (command: string) => {
+        document.execCommand(command, false, null);
+    }
+
     return (
         <div>
             <link rel="stylesheet" href="./index.css" />
 
             <input type="text" placeholder="enter title lol" />
             <br />
-            <button><b>B</b></button>
-            <button><i>i</i></button>
-            <button><u>U</u></button>
+            <button onClick={() => execCommand('bold')}><b>B</b></button>
+            <button onClick={() => execCommand('italic')}><i>i</i></button>
+            <button onClick={() => execCommand('underline')}><u>U</u></button>
             <br />
             <div contentEditable={true}></div>
         </div>
